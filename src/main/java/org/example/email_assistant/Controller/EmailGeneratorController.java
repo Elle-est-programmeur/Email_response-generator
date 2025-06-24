30602e14f442e9ad15fefd336fd022db210301ad
Elle-est-programmeur <153202178+Elle-est-programmeur@users.noreply.github.com>
2025-06-25 01:19:57 +0530

    commit-2

--- src/main/java/org/example/email_assistant/Controller/EmailGeneratorController.java
+++ src/main/java/org/example/email_assistant/Controller/EmailGeneratorController.java
@@ -3,14 +3,12 @@ package org.example.email_assistant.Controller;
 import lombok.AllArgsConstructor;
 import org.example.email_assistant.Service.EmailGeneratorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
